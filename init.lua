96588e082211d03a7e76d15249e922af73f1ee5d
team4 <2363254197@qq.com>
2019-08-10 15:00:09 +0800

    bug fix

--- init.lua
+++ init.lua
@@ -7,6 +7,6 @@ redis = require "resty.redis"
 redis_util = require "conf.util.redis_util_2"
 redis_conf = require "conf.config.redis_conf_2"
 
-mysql_config = require "config.db_config"
-mysql_ml = require "util.db_ml" 
+mysql_config = require "conf.config.db_config"
+mysql_ml = require "conf.util.db_ml" 
 mysql = require "resty.mysql"
