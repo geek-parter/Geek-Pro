5a44b10ded7a3e9c6ceef8a16ae50507ad32af58
faker <m15001006384@163.com>
2019-08-10 16:13:54 +0800

    commit fetch_cvinfo

--- src/fetch_cvinfo.lua
+++ src/fetch_cvinfo.lua
@@ -1,12 +1,59 @@
+--fetch_cvinfo
+
 ngx.req.read_body()
---获取请求体body数据
-local data,is_ok = common_util.get_data()
-local body = cjson_safe.decode(data)
---必要的参数验证
-if not body then
-    common_util.http_return(406,"body data is error")
+local data = ngx.req.get_body_data()
+
+-------------------参数检查--------------------
+local body_tb = cjson_safe.decode(data)
+if not body_tb then
+	yuqing_util.http_return(406, "invalid json")
 end
 
-if not common_util.check_args(2, body.username, body.password) then
-    common_util.http_return(406,"body data is error")
+local username = body_tb["username"]
+local password = body_tb["password"]
+
+if not username or username=="" or not password or password=="" then
+	yuqing_util.http_return(406, "invalid username/password")
 end
+
+local username_md5 = ngx.md5(username)
+local push_data = {}
+push_data[username_md5] = {
+	["username"] = username,
+	["password"] = password
+}
+
+local r = redis_util.rpush(redis_conf.yq_mysql, "username", full_key, nil, unpack(push_data))
+if not r then
+	yuqing_util.http_return(500, "push to redis error")
+end
+
+-- 当前时间
+local start_time = ngx.time()
+local time_now
+local ret_data
+while true do
+	local ret_data = redis_util.
+	if ret_data then
+		break
+	end
+	time_now = ngx.time()
+	if time_now - start_time > 60 then
+		break
+	end
+	ngx.sleep(0.5)
+end
+
+local ret_dict = {
+	["status"] = "faield",
+	["message"] = "",
+	["type"] = "",
+}
+if not ret_data then
+	ret_dict["status"] = "ok",
+	ret_dict["type"] = "",
+	ret_dict["data"] = ret_data
+end
+
+yuqing_util.http_return_json(ret_dict)
+
